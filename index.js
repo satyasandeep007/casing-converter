b16c20a3c98c6f7642e05f97c0d71db6684fd65b
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2021-10-16 12:00:10 +0530

    Feature: Flat case add

--- index.js
+++ index.js
@@ -32,4 +32,13 @@ const toKebabCase = (str) => {
         .join("-");
 };
 
-module.exports = { toCamelCase, toPascalCase, toSnakeCase, toKebabCase };
+const toFlatCase = (str) => {
+    return str
+        .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+           .map(x => x.toLowerCase())
+        //    .map(x => x.toUpperCase())
+        .join("");
+};
+
+
+module.exports = { toCamelCase, toPascalCase, toSnakeCase, toKebabCase, toFlatCase };
